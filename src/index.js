cee3daf01ce211af7c679122348315b87983c84a
Dm3583 <pankovdm@gmail.com>
2020-12-02 17:10:58 +0200

    add cards markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import menu from "./data/menu.json";
+import menuCardTemplate from "./templates-hbs/food-card-markup.hbs";
+
+
+const refMenuList = document.querySelector('.js-menu');
+const menuMarkup = menuCardTemplate(menu);
+
+
+refMenuList.insertAdjacentHTML('afterbegin', menuMarkup);
+
+
+
